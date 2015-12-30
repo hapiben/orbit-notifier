e767bc280094f2be4f2668b192ff8ce79babba27
Benedict Aluan <benedict.aluan@gmail.com>
2015-12-30 16:07:42 +1300

    Added basic views.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = t('app.title')
+
+    return base_title if page_title.empty?
+    return "#{base_title} | #{page_title}"
+  end
 end
--- features/support/pages/home_page.rb
+++ features/support/pages/home_page.rb
@@ -2,5 +2,5 @@ class HomePage < SitePrism::Page
   set_url '/'
 
   element :logo, 'a.logo'
-  element :description, 'p.description'
+  element :description, '.description'
 end
